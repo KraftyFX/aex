25a39a778ad8802b22fa922d83eee109646cf4a5
Zack Lovatt <zack@zacklovatt.com>
2021-03-08 16:50:30 -0800

    Fixing Windows CEP path

--- cep/webpack/contants.ts
+++ cep/webpack/contants.ts
@@ -2,11 +2,13 @@ import path = require('path');
 const { name } = require('../../cep/package.json');
 
 const system_ae_extension_root =
-    process.platform === 'win32' ? `${process.env.APPDATA}\\cep\\extensions\\${name}` : `/Library/Application Support/Adobe/CEP/extensions/${name}`;
+    process.platform === 'win32'
+        ? `${process.env.APPDATA}\\Adobe\\CEP\\extensions\\${name}`
+        : `/Library/Application Support/Adobe/CEP/extensions/${name}`;
 
 const user_ae_extension_root =
     process.platform === 'win32'
-        ? `${process.env.APPDATA}\\cep\\extensions\\${name}`
+        ? `${process.env.APPDATA}\\Adobe\\CEP\\extensions\\${name}`
         : `${process.env.HOME}/Library/Application Support/Adobe/CEP/extensions/${name}`;
 
 export const paths = {
