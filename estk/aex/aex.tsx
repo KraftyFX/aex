b7b6bdf928170bfb1406a39fbf2cab5ad94873b9
Zack Lovatt <zack@zacklovatt.com>
2021-03-10 15:12:27 -0800

    Adding layer attributes

--- estk/aex/aex.tsx
+++ estk/aex/aex.tsx
@@ -51,7 +51,7 @@ function aex(options: AexOptions) {
             const preserveNestedResolution = getModifiedValue(comp.preserveNestedResolution, false);
             const resolutionFactor = getModifiedValue(comp.resolutionFactor, [1, 1]);
             const workAreaStart = getModifiedValue(comp.workAreaStart, 0);
-            const workAreaDuration = comp.workAreaDuration === duration ? undefined : comp.workAreaDuration;
+            const workAreaDuration = getModifiedValue(comp.workAreaDuration, duration);
 
             /**
              * @todo Add AexOption to preserve project folder structure.
@@ -150,8 +150,120 @@ function aex(options: AexOptions) {
             return markerData;
         },
         visitLayer(layer: Layer): AexLayer {
+            const containingComp = layer.containingComp;
+
+            const name = layer.name;
+            const label = layer.label;
+            let layerType = 'Layer' as AexLayerType;
+
+            const comment = getModifiedValue(layer.comment, '');
+            let hasVideo = getModifiedValue(layer.hasVideo, true);
+            const inPoint = getModifiedValue(layer.inPoint, 0);
+            const outPoint = getModifiedValue(layer.outPoint, containingComp.duration);
+            const startTime = getModifiedValue(layer.startTime, 0);
+            const stretch = getModifiedValue(layer.stretch, 100);
+            const nullLayer = getModifiedValue(layer.nullLayer, false);
+            const shy = getModifiedValue(layer.shy, false);
+            const solo = getModifiedValue(layer.solo, false);
+
+            const parentLayerIndex = layer.parent ? layer.parent.index : undefined;
+
+            let avAttributes = {} as AexAVLayerAttributes;
+            if (aeq.isAVLayer(layer)) {
+                layerType = 'AVLayer';
+
+                /** @todo Handle track matte */
+                /** @todo Handle source */
+                const source = layer.source;
+
+                const adjustmentLayer = getModifiedValue(layer.adjustmentLayer, false);
+                const audioEnabled = getModifiedValue(layer.audioEnabled, true);
+                const autoOrient = getModifiedValue(layer.autoOrient, AutoOrientType.NO_AUTO_ORIENT);
+                const blendingMode = getModifiedValue(layer.blendingMode, BlendingMode.NORMAL);
+                const collapseTransformation = getModifiedValue(layer.collapseTransformation, false);
+                const effectsActive = getModifiedValue(layer.effectsActive, true);
+                const environmentLayer = getModifiedValue(layer.environmentLayer, false);
+                const frameBlending = getModifiedValue(layer.frameBlending, false);
+                const frameBlendingType = frameBlending ? getModifiedValue(layer.frameBlendingType, FrameBlendingType.NO_FRAME_BLEND) : undefined;
+                const guideLayer = getModifiedValue(layer.guideLayer, false);
+                const motionBlur = getModifiedValue(layer.motionBlur, false);
+                const preserveTransparency = getModifiedValue(layer.preserveTransparency, false);
+                const quality = getModifiedValue(layer.quality, LayerQuality.BEST);
+                const samplingQuality = getModifiedValue(layer.samplingQuality, LayerSamplingQuality.BILINEAR);
+                const threeDLayer = getModifiedValue(layer.threeDLayer, false);
+                const timeRemapEnabled = getModifiedValue(layer.timeRemapEnabled, false);
+                const trackMatteType = getModifiedValue(layer.trackMatteType, TrackMatteType.NO_TRACK_MATTE);
+
+                avAttributes = {
+                    adjustmentLayer,
+                    audioEnabled,
+                    autoOrient,
+                    blendingMode,
+                    collapseTransformation,
+                    effectsActive,
+                    environmentLayer,
+                    frameBlending,
+                    frameBlendingType,
+                    guideLayer,
+                    motionBlur,
+                    preserveTransparency,
+                    quality,
+                    samplingQuality,
+                    threeDLayer,
+                    timeRemapEnabled,
+                    trackMatteType,
+                };
+            }
+
+            if (aeq.isShapeLayer(layer)) {
+                layerType = 'ShapeLayer';
+            }
+
+            let lightAttributes = {} as AexLightLayerAttributes;
+            if (aeq.isLightLayer(layer)) {
+                hasVideo = getModifiedValue(layer.hasVideo, false);
+                layerType = 'LightLayer';
+
+                const lightType = layer.lightType;
+                lightAttributes = {
+                    lightType,
+                };
+            }
+
+            let textAttributes = {} as AexTextLayerAttributes;
+            if (aeq.isTextLayer(layer)) {
+                layerType = 'TextLayer';
+
+                const threeDPerChar = layer.threeDLayer ? getModifiedValue(layer.threeDPerChar, false) : undefined;
+                textAttributes = {
+                    threeDPerChar,
+                };
+            }
+
+            if (aeq.isCameraLayer(layer)) {
+                hasVideo = getModifiedValue(layer.hasVideo, false);
+                layerType = 'CameraLayer';
+            }
+
             return {
-                props: [],
+                name,
+                label,
+                layerType,
+
+                comment,
+                hasVideo,
+                inPoint,
+                outPoint,
+                startTime,
+                stretch,
+                nullLayer,
+                shy,
+                solo,
+                parentLayerIndex,
+
+                ...avAttributes,
+                ...lightAttributes,
+                ...textAttributes,
             };
         },
     };
--- estk/aex/interfaces.tsx
+++ estk/aex/interfaces.tsx
@@ -1,5 +1,7 @@
 type Serializable = Project | CompItem | Layer | Property<any>;
 
+type AexLayerType = 'Layer' | 'CameraLayer' | 'LightLayer' | 'AVLayer' | 'ShapeLayer' | 'TextLayer';
+
 interface AexOptions {}
 
 interface AexProject {
@@ -43,7 +45,48 @@ interface AexComp {
 }
 
 interface AexLayer {
-    props: {}[];
+    name: string;
+    label: number;
+    comment: string;
+    hasVideo: boolean;
+    inPoint: number;
+    outPoint: number;
+    startTime: number;
+    stretch: number;
+    nullLayer: boolean;
+    shy: boolean;
+    solo: boolean;
+    parentLayerIndex: number;
+
+    layerType: AexLayerType;
+}
+
+interface AexAVLayerAttributes {
+    adjustmentLayer: boolean;
+    audioEnabled: boolean;
+    autoOrient: AutoOrientType;
+    blendingMode: BlendingMode;
+    collapseTransformation: boolean;
+    effectsActive: boolean;
+    environmentLayer: boolean;
+    frameBlending: boolean;
+    frameBlendingType: FrameBlendingType;
+    guideLayer: boolean;
+    motionBlur: boolean;
+    preserveTransparency: boolean;
+    quality: LayerQuality;
+    samplingQuality: LayerSamplingQuality;
+    threeDLayer: boolean;
+    timeRemapEnabled: boolean;
+    trackMatteType: TrackMatteType;
+}
+
+interface AexLightLayerAttributes {
+    lightType: LightType;
+}
+
+interface AexTextLayerAttributes {
+    threeDPerChar: boolean;
 }
 
 interface AexProperty {}
