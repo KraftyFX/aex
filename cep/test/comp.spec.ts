d1b2590a6bbc8572f0c78ac924bbb063961260ae
Zack Lovatt <zack@zacklovatt.com>
2021-03-19 14:36:08 -0700

    Splitting comp marker tests into separate tests for devx

--- cep/test/comp.spec.ts
+++ cep/test/comp.spec.ts
@@ -41,62 +41,66 @@ describe('Comp', function () {
             ]);
     });
 
-    it(`Can parse comp markers`, async () => {
-        await openProject('testAssets/comp_markers.aep');
+    describe('Comp Markers', async () => {
+        let result: any;
 
-        const result = await aex().toObjectWithAeObject(AeObject.Project);
+        before(async () => {
+            await openProject('testAssets/comp_markers.aep');
+            result = await aex().toObjectWithAeObject(AeObject.Project);
+            console.log('comp_markers', result);
+        });
 
-        console.log('comp_markers-comp', result);
+        it(`Can parse detailed markers`, async () => {
+            expect(result.comps[0])
+                .property('markers')
+                .to.eql([
+                    {
+                        duration: 0.2,
+                        time: 0.16666666666667,
+                    },
+                    {
+                        comment: 'Some Comment',
+                        duration: 1,
+                        label: 4,
+                        protectedRegion: true,
+                        time: 0.46666666666667,
+                    },
+                    {
+                        label: 3,
+                        time: 0.78333333333333,
+                    },
+                    {
+                        comment: 'banana',
+                        time: 1.83333333333333,
+                    },
+                    {
+                        duration: 0.33333333333333,
+                        protectedRegion: true,
+                        time: 3.55,
+                    },
+                ]);
+        });
 
-        expect(result).property('comps').to.be.of.length(2);
-
-        expect(result.comps[0])
-            .property('markers')
-            .to.eql([
-                {
-                    duration: 0.2,
-                    time: 0.16666666666667,
-                },
-                {
-                    comment: 'Some Comment',
-                    duration: 1,
-                    label: 4,
-                    protectedRegion: true,
-                    time: 0.46666666666667,
-                },
-                {
-                    label: 3,
-                    time: 0.78333333333333,
-                },
-                {
-                    comment: 'banana',
-                    time: 1.83333333333333,
-                },
-                {
-                    duration: 0.33333333333333,
-                    protectedRegion: true,
-                    time: 3.55,
-                },
-            ]);
-
-        expect(result.comps[1])
-            .property('markers')
-            .to.eql([
-                {
-                    time: 0.16666666666667,
-                },
-                {
-                    time: 0.46666666666667,
-                },
-                {
-                    time: 0.78333333333333,
-                },
-                {
-                    time: 1.83333333333333,
-                },
-                {
-                    time: 3.55,
-                },
-            ]);
+        it(`Can parse simple markers`, async () => {
+            expect(result.comps[1])
+                .property('markers')
+                .to.eql([
+                    {
+                        time: 0.16666666666667,
+                    },
+                    {
+                        time: 0.46666666666667,
+                    },
+                    {
+                        time: 0.78333333333333,
+                    },
+                    {
+                        time: 1.83333333333333,
+                    },
+                    {
+                        time: 3.55,
+                    },
+                ]);
+        });
     });
 });
