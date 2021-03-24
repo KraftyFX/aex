d5b7fdce48919182fe855cf7e4e6a2fccfc77c87
Zack Lovatt <zack@zacklovatt.com>
2021-03-23 21:20:34 -0700

    Adding test for materialOption and geometryOption

--- cep/test/layer-basic.spec.ts
+++ cep/test/layer-basic.spec.ts
@@ -1,5 +1,5 @@
 import { AeObject, aex } from './aex';
-import { AEX_AV_LAYER, AEX_CAMERA_LAYER, AEX_LIGHT_LAYER, AEX_TEXT_LAYER } from './constants';
+import { AEX_AV_LAYER, AEX_CAMERA_LAYER, AEX_LIGHT_LAYER, AEX_SHAPE_LAYER, AEX_TEXT_LAYER } from './constants';
 import { cleanupAeqIpc, cleanupAex, evalAexIntoESTK, openProject } from './csinterface';
 import { assertAreEqual } from './utils';
 
@@ -113,7 +113,6 @@ describe('Basic Layer Attributes', function () {
             samplingQuality: 4813,
             shy: true,
             source: 'null 1:50',
-            threeDLayer: true,
             transform: {},
             type: AEX_AV_LAYER,
         });
@@ -154,6 +153,127 @@ describe('Basic Layer Attributes', function () {
                 },
             },
             type: AEX_AV_LAYER,
+        });
+    });
+
+    it(`Can parse 3d layers`, async () => {
+        assertAreEqual(result.comps[0].layers[7], {
+            collapseTransformation: true,
+            effects: [],
+            geometryOption: {
+                matchName: 'ADBE Extrsn Options Group',
+                properties: [
+                    {
+                        keys: [],
+                        matchName: 'ADBE Bevel Styles',
+                        name: 'Bevel Style',
+                        value: 2,
+                    },
+                    {
+                        keys: [],
+                        matchName: 'ADBE Bevel Depth',
+                        name: 'Bevel Depth',
+                        value: 1,
+                    },
+                    {
+                        keys: [],
+                        matchName: 'ADBE Hole Bevel Depth',
+                        name: 'Hole Bevel Depth',
+                        value: 2,
+                    },
+                    {
+                        keys: [],
+                        matchName: 'ADBE Extrsn Depth',
+                        name: 'Extrusion Depth',
+                        value: 3,
+                    },
+                ],
+            },
+            label: 8,
+            markers: [],
+            masks: [],
+            materialOption: {
+                matchName: 'ADBE Material Options Group',
+                properties: [
+                    {
+                        keys: [],
+                        matchName: 'ADBE Casts Shadows',
+                        name: 'Casts Shadows',
+                        value: 1,
+                    },
+                    {
+                        keys: [],
+                        matchName: 'ADBE Accepts Shadows',
+                        name: 'Accepts Shadows',
+                        value: 2,
+                    },
+                    {
+                        keys: [],
+                        matchName: 'ADBE Accepts Lights',
+                        name: 'Accepts Lights',
+                        value: 0,
+                    },
+                    {
+                        keys: [],
+                        matchName: 'ADBE Appears in Reflections',
+                        name: 'Appears in Reflections',
+                        value: 2,
+                    },
+                    {
+                        keys: [],
+                        matchName: 'ADBE Ambient Coefficient',
+                        name: 'Ambient',
+                        value: 1,
+                    },
+                    {
+                        keys: [],
+                        matchName: 'ADBE Diffuse Coefficient',
+                        name: 'Diffuse',
+                        value: 2,
+                    },
+                    {
+                        keys: [],
+                        matchName: 'ADBE Specular Coefficient',
+                        name: 'Specular Intensity',
+                        value: 3,
+                    },
+                    {
+                        keys: [],
+                        matchName: 'ADBE Shininess Coefficient',
+                        name: 'Specular Shininess',
+                        value: 4,
+                    },
+                    {
+                        keys: [],
+                        matchName: 'ADBE Metal Coefficient',
+                        name: 'Metal',
+                        value: 5,
+                    },
+                    {
+                        keys: [],
+                        matchName: 'ADBE Reflection Coefficient',
+                        name: 'Reflection Intensity',
+                        value: 6,
+                    },
+                    {
+                        keys: [],
+                        matchName: 'ADBE Glossiness Coefficient',
+                        name: 'Reflection Sharpness',
+                        value: 7,
+                    },
+                    {
+                        keys: [],
+                        matchName: 'ADBE Fresnel Coefficient',
+                        name: 'Reflection Rolloff',
+                        value: 8,
+                    },
+                ],
+            },
+            name: '3d',
+            source: '',
+            threeDLayer: true,
+            transform: {},
+            type: AEX_SHAPE_LAYER,
         });
     });
 });
