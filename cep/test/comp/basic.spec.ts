7b2198e1e384add0cffcf0f255e4828f73a662eb
Rafi Khan <rafikhan@gmail.com>
2021-03-31 08:18:19 -0700

    Moving comp tests into their own folder

--- cep/test/comp/basic.spec.ts
+++ cep/test/comp/basic.spec.ts
@@ -1,7 +1,7 @@
-import { AeObject, aex } from './aex';
-import { AEX_COMP_ITEM } from './constants';
-import { cleanupAeqIpc, cleanupAex, evalAexIntoESTK, openProject } from './csinterface';
-import { assertAreEqual } from './utils';
+import { AeObject, aex } from '../aex';
+import { AEX_COMP_ITEM } from '../constants';
+import { cleanupAeqIpc, cleanupAex, evalAexIntoESTK, openProject } from '../csinterface';
+import { assertAreEqual } from '../utils';
 
 describe('Comp', function () {
     this.slow(500);
