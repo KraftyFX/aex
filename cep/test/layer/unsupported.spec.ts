8eea9015717ab255e83d3f8a04731f881ab7a414
Zack Lovatt <zack@zacklovatt.com>
2021-04-07 00:19:24 -0700

    Adding test for unsupported properties

---
+++ cep/test/layer/unsupported.spec.ts
@@ -0,0 +1,65 @@
+import { expect } from 'chai';
+import { AeObject, aex } from '../aex';
+import { cleanupAex, evalAexIntoEstk, openProject } from '../csinterface';
+
+describe('Unsupported Properties', function () {
+    this.slow(500);
+    this.timeout(2000);
+
+    before(async () => {
+        await evalAexIntoEstk();
+        await openProject('testAssets/property_unsupported.aep');
+    });
+
+    after(async () => {
+        await cleanupAex();
+    });
+
+    it(`Can get log of serialization failures`, async () => {
+        const result = await aex().fromAeObject(AeObject.ActiveComp, {
+            unspportedPropertyBehavior: 'log',
+        });
+
+        console.log('unsupported_log', result);
+
+        expect(result.object).to.be.ok;
+        expect(result.log[0].message).to.contain('is unsupported');
+    });
+
+    it(`Can skip serialization failures`, async () => {
+        const result = await aex().fromAeObject(AeObject.ActiveComp, {
+            unspportedPropertyBehavior: 'skip',
+        });
+
+        console.log('unsupported_skip', result);
+
+        expect(result.object).to.be.ok;
+        expect(result.log).to.be.empty;
+    });
+
+    it(`Can throw on serialization failures`, async () => {
+        try {
+            await aex().fromAeObject(AeObject.ActiveComp, {
+                unspportedPropertyBehavior: 'throw',
+            });
+
+            expect.fail(`Test should have thrown but it completed.`);
+        } catch (e) {
+            console.log('unsupported_throw');
+            console.dir(e);
+            expect(e.isEstkError).to.be.true;
+            expect(e.message).to.contain('is unsupported');
+        }
+    });
+
+    it(`Can get metadata on serialization failures`, async () => {
+        const result = await aex().fromAeObject(AeObject.ActiveComp, {
+            unspportedPropertyBehavior: 'metadata',
+        });
+
+        console.log('unsupported_metadata', result);
+
+        expect(result.object).to.be.ok;
+        expect(result.object.layers[0].layerStyles.properties[1].properties[0]).to.not.have.property('value');
+    });
+});
