46fcd9942df64ac4653d16e57ec412d25cde1565
Rafi Khan <rafikhan@gmail.com>
2021-04-10 19:23:17 -0700

    Readability: renaming variables during property group flattening

--- estk/aex/layer.tsx
+++ estk/aex/layer.tsx
@@ -51,34 +51,34 @@ function _getLayerStyles(styleGroup: PropertyGroup, state: AexState) {
     return styles;
 }
 
-function _getFlatPropertyGroup(property: PropertyGroup, state: AexState): AexPropertyGroup[] {
+function _getFlatPropertyGroup(propertyGroup: PropertyGroup, state: AexState): AexPropertyGroup[] {
     const result = [];
 
-    forEachPropertyInGroup(property, (childProperty: PropertyGroup) => {
+    forEachPropertyInGroup(propertyGroup, (childPropertyGroup: PropertyGroup) => {
         let contents;
 
-        if (childProperty.matchName === 'ADBE Vector Group') {
-            const contentsGroup = childProperty.property('ADBE Vectors Group');
-            contents = _getFlatPropertyGroup(contentsGroup as PropertyGroup, state);
+        if (childPropertyGroup.matchName === 'ADBE Vector Group') {
+            const vectorsGroup = childPropertyGroup.property('ADBE Vectors Group') as PropertyGroup;
+            contents = _getFlatPropertyGroup(vectorsGroup, state);
         }
 
-        const propertyData = getPropertyGroup(childProperty as PropertyGroup, state);
+        const propertyData = getPropertyGroup(childPropertyGroup, state);
 
         /**
          * @todo
-         * getPropertyGroup() is set up so that if there's no data, it doesn't return the group at al, statel.
+         * getPropertyGroup() is set up so that if there's no data, it doesn't return the group at all.
          * This means that if there's a layer effect that has defaults, effects: [] will be empty
          * However, we _always_ want to return the effects if they're present, even if the properties are default.
          *
          * This approach below sucks because we're repeating work.
          * Best would be to still return enabled/matchName/name, with an empty properties array.
-         * This could be accomplished by adding an optional parameter to getPropertyGroup for whether to return undefined if empty or n, stateot
+         * This could be accomplished by adding an optional parameter to getPropertyGroup for whether to return undefined if empty or not
          * In cases like masks & effects, this would be false. Otherwise true.
          */
         const properties = propertyData ? propertyData.properties : undefined;
 
-        const { name, matchName } = childProperty;
-        const enabled = getModifiedValue(childProperty.enabled, true);
+        const { name, matchName } = childPropertyGroup;
+        const enabled = getModifiedValue(childPropertyGroup.enabled, true);
 
         result.push({
             name,
