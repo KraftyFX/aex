efaf6e4b3dbdefcea89a9e52bf085af6aa2517b7
Zack Lovatt <zack@zacklovatt.com>
2021-04-11 11:20:32 -0700

    Adding test for puppet pins

--- cep/test/layer/effects.spec.ts
+++ cep/test/layer/effects.spec.ts
@@ -7,14 +7,17 @@ describe('Layer Effects', function () {
     this.slow(500);
     this.timeout(2000);
 
-    let comp: any;
+    let puppetComp: any;
+    let simpleComp: any;
 
     before(async () => {
         await evalAexIntoEstk();
         await openProject('testAssets/layer_effects.aep');
-        const result = await aex().fromAeObject(AeObject.ActiveComp);
-        comp = result.object;
-        console.log('layer_effects', comp);
+        const result = await aex().fromAeObject(AeObject.Project);
+        const project = result.object;
+        puppetComp = project.comps[0];
+        simpleComp = project.comps[1];
+        console.log('layer_effects', project);
     });
 
     after(async () => {
@@ -22,14 +25,14 @@ describe('Layer Effects', function () {
     });
 
     it('Can parse simple unmodified effect', async () => {
-        assertAreEqual(comp.layers[0].effects[0], {
+        assertAreEqual(simpleComp.layers[0].effects[0], {
             matchName: 'ADBE Fill',
             name: 'Fill - Default',
         });
     });
 
     it('Can parse simple modified effect', async () => {
-        assertAreEqual(comp.layers[0].effects[1], {
+        assertAreEqual(simpleComp.layers[0].effects[1], {
             matchName: 'ADBE Fill',
             name: 'Fill - Modified',
             properties: [
@@ -80,7 +83,7 @@ describe('Layer Effects', function () {
     });
 
     it('Can parse effect compositing options', async () => {
-        assertAreEqual(comp.layers[0].effects[2], {
+        assertAreEqual(simpleComp.layers[0].effects[2], {
             matchName: 'ADBE Fill',
             name: 'Fill - Compositing Options',
             properties: [
@@ -119,7 +122,7 @@ describe('Layer Effects', function () {
     });
 
     it('Can parse default expression controls', async () => {
-        assertAreEqual(comp.layers[1].effects, [
+        assertAreEqual(simpleComp.layers[1].effects, [
             {
                 name: '3D Point Control',
                 matchName: 'ADBE Point3D Control',
@@ -156,7 +159,7 @@ describe('Layer Effects', function () {
     });
 
     it('Can parse modified expression controls', async () => {
-        assertAreEqual(comp.layers[2].effects, [
+        assertAreEqual(simpleComp.layers[2].effects, [
             {
                 name: '3D Point Control',
                 matchName: 'ADBE Point3D Control',
@@ -265,7 +268,7 @@ describe('Layer Effects', function () {
     });
 
     it('Can parse nested effect groups', async () => {
-        assertAreEqual(comp.layers[3].effects[0], {
+        assertAreEqual(simpleComp.layers[3].effects[0], {
             matchName: 'ADBE Fractal Noise',
             name: 'Fractal Noise',
             properties: [
@@ -275,6 +278,132 @@ describe('Layer Effects', function () {
                     name: 'Scale',
                     type: AEX_ONED_PROPERTY,
                     value: 123,
+                },
+            ],
+        });
+    });
+
+    it('Can parse puppet pins', async () => {
+        assertAreEqual(puppetComp.layers[0].effects[0], {
+            name: 'Puppet',
+            matchName: 'ADBE FreePin3',
+            properties: [
+                {
+                    matchName: 'ADBE FreePin3 ARAP Group',
+                    properties: [
+                        {
+                            matchName: 'ADBE FreePin3 Mesh Group',
+                            properties: [
+                                {
+                                    matchName: 'ADBE FreePin3 Mesh Atom',
+                                    name: 'Custom Mesh Name',
+                                    properties: [
+                                        {
+                                            type: 'aex:property:oned',
+                                            name: 'Triangles',
+                                            matchName: 'ADBE FreePin3 Mesh Tri Count',
+                                            value: 50,
+                                            keys: [],
+                                        },
+                                        {
+                                            matchName: 'ADBE FreePin3 PosPins',
+                                            properties: [
+                                                {
+                                                    matchName: 'ADBE FreePin3 PosPin Atom',
+                                                    name: 'Puppet Pin 2',
+                                                    properties: [
+                                                        {
+                                                            type: 'aex:property:oned',
+                                                            name: 'Vertex Index',
+                                                            matchName: 'ADBE FreePin3 PosPin Vtx Index',
+                                                            value: 12,
+                                                            keys: [],
+                                                        },
+                                                        {
+                                                            type: 'aex:property:twod',
+                                                            name: 'Position',
+                                                            matchName: 'ADBE FreePin3 PosPin Position',
+                                                            value: [13, 482],
+                                                            keys: [],
+                                                        },
+                                                    ],
+                                                },
+                                                {
+                                                    matchName: 'ADBE FreePin3 PosPin Atom',
+                                                    name: 'Custom Pin 1',
+                                                    properties: [
+                                                        {
+                                                            type: 'aex:property:oned',
+                                                            name: 'Vertex Index',
+                                                            matchName: 'ADBE FreePin3 PosPin Vtx Index',
+                                                            value: 13,
+                                                            keys: [],
+                                                        },
+                                                        {
+                                                            type: 'aex:property:twod',
+                                                            name: 'Position',
+                                                            matchName: 'ADBE FreePin3 PosPin Position',
+                                                            value: [15, 14],
+                                                            keys: [
+                                                                {
+                                                                    value: [15, 14],
+                                                                    time: 0,
+                                                                    interpolationType: {},
+                                                                    temporalEase: {
+                                                                        inEase: [
+                                                                            {
+                                                                                influence: 16.666666667,
+                                                                                speed: 0,
+                                                                            },
+                                                                        ],
+                                                                        outEase: [
+                                                                            {
+                                                                                influence: 16.666666667,
+                                                                                speed: 147.692307692308,
+                                                                            },
+                                                                        ],
+                                                                    },
+                                                                    spatialTangent: {
+                                                                        inTangent: [0, 0],
+                                                                        outTangent: [0, 0],
+                                                                    },
+                                                                    spatialContinuous: true,
+                                                                },
+                                                                {
+                                                                    value: [15, 254],
+                                                                    time: 1.625,
+                                                                    interpolationType: {},
+                                                                    temporalEase: {
+                                                                        inEase: [
+                                                                            {
+                                                                                influence: 16.666666667,
+                                                                                speed: 147.692307692308,
+                                                                            },
+                                                                        ],
+                                                                        outEase: [
+                                                                            {
+                                                                                influence: 16.666666667,
+                                                                                speed: 0,
+                                                                            },
+                                                                        ],
+                                                                    },
+                                                                    spatialTangent: {
+                                                                        inTangent: [0, 0],
+                                                                        outTangent: [0, 0],
+                                                                    },
+                                                                    spatialContinuous: true,
+                                                                },
+                                                            ],
+                                                        },
+                                                    ],
+                                                },
+                                            ],
+                                        },
+                                    ],
+                                },
+                            ],
+                        },
+                    ],
                 },
             ],
         });
