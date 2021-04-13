6b8cc12b2d4d623137af8c8793fbf5ecf9741535
Zack Lovatt <zack@zacklovatt.com>
2021-04-13 12:06:22 -0700

    Removing already-resolved todo comment

--- estk/aex/layer/layerutil.tsx
+++ estk/aex/layer/layerutil.tsx
@@ -19,18 +19,6 @@ function getUnnestedPropertyGroup<T extends AexPropertyGroup = AexPropertyGroup>
             properties: null,
         } as T;
 
-        /**
-         * @todo
-         * getPropertyGroup() is set up so that if there's no data, it doesn't return the group at all.
-         * This means that if there's a layer effect that has defaults, effects: [] will be empty
-         * However, we _always_ want to return the effects if they're present, even if the properties are default.
-         *
-         * This approach below sucks because we're repeating work.
-         * Best would be to still return enabled/matchName/name, with an empty properties array.
-         * This could be accomplished by adding an optional parameter to getPropertyGroup for whether to return undefined if empty or not
-         * In cases like masks & effects, this would be false. Otherwise true.
-         */
-
         callback(childPropertyGroup, aexGroup);
 
         if (aexGroup.properties === null) {
