1a1aacdfa0af5df61e5da326aefffbbc8c517afb
Zack Lovatt <zack@zacklovatt.com>
2021-04-15 18:18:01 -0700

    Setting AVLayerAttributes onto createFootageLayer

--- estk/aex/layer/footage.tsx
+++ estk/aex/layer/footage.tsx
@@ -13,10 +13,6 @@ function getFootageLayer(layer: AVLayer, state: AexState): AexFootageLayer {
 function createFootageLayer(comp: CompItem, aexFootageLayer: AexFootageLayer, state: AexState) {
     const aexSource = aexFootageLayer.source;
 
-    // try to find the project item, using the ID and type
-    // if it doesn't exist, make a new one
-    // if it does, use that one
-
     let sourceItem: AVItem;
 
     if (aexSource.type === AEX_COMP_ITEM) {
@@ -26,13 +22,11 @@ function createFootageLayer(comp: CompItem, aexFootageLayer: AexFootageLayer, st
     }
 
     const layer = comp.layers.add(sourceItem);
+    _setAVLayerAttributes(layer, aexFootageLayer, state);
 
-    // const layer = comp.layers.add();
-    // _setAVLayerAttributes(layer, aexFootageLayer, state);
     /**
      * @todo
      *
-     * - source
      * - trackers
      */
 }
