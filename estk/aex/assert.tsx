4a02fc17ef0a62566102389b4740f0d84bc29e71
Rafi Khan <rafikhan@gmail.com>
2021-04-17 17:07:36 -0700

    Movin property type

--- estk/aex/assert.tsx
+++
@@ -1,5 +0,0 @@
-function assertIsDefined(o: any, name?: string) {
-    if (aeq.isNullOrUndefined(o)) {
-        throw new Error(`${name || 'parameter'} is null or undefined`);
-    }
-}
--- estk/aex/common/propertyutil.tsx
+++ estk/aex/common/propertyutil.tsx
@@ -49,32 +49,3 @@ function getUnsupportedProperty(property: Property<UnknownPropertyType>, aexProp
             return aexProperty;
     }
 }
-
-function _getPropertyType(property: Property<UnknownPropertyType>): AexPropertyType {
-    switch (property.propertyValueType) {
-        case PropertyValueType.OneD:
-        case PropertyValueType.MASK_INDEX:
-        case PropertyValueType.LAYER_INDEX:
-            return AEX_ONED_PROPERTY;
-        case PropertyValueType.TwoD:
-        case PropertyValueType.TwoD_SPATIAL:
-            return AEX_TWOD_PROPERTY;
-        case PropertyValueType.ThreeD:
-        case PropertyValueType.ThreeD_SPATIAL:
-            return AEX_THREED_PROPERTY;
-        case PropertyValueType.COLOR:
-            return AEX_COLOR_PROPERTY;
-        case PropertyValueType.SHAPE:
-            return AEX_SHAPE_PROPERTY;
-        case PropertyValueType.TEXT_DOCUMENT:
-            return AEX_TEXTDOCUMENT_PROPERTY;
-        case PropertyValueType.MARKER:
-            return AEX_MARKER_PROPERTY;
-        case PropertyValueType.NO_VALUE:
-            return AEX_NONE_PROPERTY;
-        case PropertyValueType.CUSTOM_VALUE:
-            return AEX_CUSTOM_PROPERTY;
-        default:
-            throw new Error(`Unsupported property type "${property.name}" ${property.propertyValueType}`);
-    }
-}
