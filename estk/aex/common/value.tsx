1a4538d5577849e71f5c302fd3c2d8432e49d8f8
Rafi Khan <rafikhan@gmail.com>
2021-04-18 09:30:24 -0700

    Moving value code together

--- estk/aex/common/value.tsx
+++ estk/aex/common/value.tsx
@@ -1,3 +1,38 @@
+/**
+ * Gets the value of a property if and only if another boolean property is set
+ * that dictates if and how it should be read.
+ *
+ * @param shouldRead Property from AfterEffects that decides if the value should be read.
+ * @param callback Function that gets the value that should be serialized
+ * @param aeDefaultValue The expected default value provided by AE for the property
+ * @returns The property value if and only if the property exists and is
+ * set to something other than the default.
+ */
+function getBoundModifiedValue<T>(shouldRead: boolean, callback: () => T, aeDefaultValue: T): T | undefined {
+    /**
+     * @todo Add AexOption for whether to return default values, vs return undefined
+     */
+    return shouldRead ? getModifiedValue<T>(callback(), aeDefaultValue) : undefined;
+}
+
+/**
+ * Gets the value of a property if it's different than the internal AE
+ * default. This is useful b/c it helps keep the serialized objects
+ * small.
+ *
+ * @param value Property value from AfterEffects
+ * @param aeDefaultValue The expected default value provided by AE for the property
+ * @returns The property value if and only if the property exists and is
+ * set to something other than the default.
+ */
+function getModifiedValue<T>(value: T, aeDefaultValue: T): T | undefined {
+    if (aeq.isNullOrUndefined(value)) {
+        return undefined;
+    }
+
+    return _isEqual(value, aeDefaultValue) ? undefined : getRoundedValue(value);
+}
+
 function _isObjectEqual(a: object, b: object): boolean {
     if (typeof b !== 'object') {
         return false;
@@ -76,22 +111,4 @@ function roundArray(values: number[], precision = 4): number[] {
     return aeq.arrayEx(values).map((value) => {
         return roundNumber(value, precision);
     });
-}
-
-/**
- * Gets the value of a property if it's different than the internal AE
- * default. This is useful b/c it helps keep the serialized objects
- * small.
- *
- * @param value Property value from AfterEffects
- * @param aeDefaultValue The expected default value provided by AE for the property
- * @returns The property value if and only if the property exists and is
- * set to something other than the default.
- */
-function getModifiedValue<T>(value: T, aeDefaultValue: T): T | undefined {
-    if (aeq.isNullOrUndefined(value)) {
-        return undefined;
-    }
-
-    return _isEqual(value, aeDefaultValue) ? undefined : getRoundedValue(value);
 }
