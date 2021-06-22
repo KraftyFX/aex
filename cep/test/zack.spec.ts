0e02ac813e52452b1b9dbdd4068fa25691e74824
Zack Lovatt <zack@zacklovatt.com>
2021-06-22 10:19:37 -0700

    Adding zack tests back in

---
+++ cep/test/zack.spec.ts
@@ -0,0 +1,31 @@
+import { expect } from 'chai';
+import { AeObject, aex } from './aex';
+import { cleanupAex, evalAexIntoEstk } from './csinterface';
+
+describe.only('Zack Test Stuff', function () {
+    this.slow(500);
+    this.timeout(5000);
+
+    before(async () => {
+        await evalAexIntoEstk();
+    });
+
+    after(async () => {
+        await cleanupAex();
+    });
+
+    /** Data dumps */
+    it(`Unsophisticated test to check comp data parsing`, async () => {
+        const result = await aex().get(AeObject.ActiveComp);
+
+        console.log('activecomp', result.object);
+        expect(result.object);
+    });
+
+    it(`Unsophisticated test to check project data parsing`, async () => {
+        const result = await aex().get(AeObject.Project);
+
+        console.log('full project', result.object);
+        expect(result.object);
+    });
+});
