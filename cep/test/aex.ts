d0c7c6f0dd714302c024288de164dd02168a18d1
Zack Lovatt <zack@zacklovatt.com>
2021-06-22 11:13:56 -0700

    Add aex test support for specific layers

--- cep/test/aex.ts
+++ cep/test/aex.ts
@@ -1,9 +1,10 @@
 import { AexObject, AexOptions, AexPrescanResult, AexResult } from './constants';
 import { getEvalScriptResult } from './csinterface';
 
-export enum AeObject {
-    ActiveComp = 'app.project.activeItem',
-    Project = 'app.project',
+export const AeObject = {
+    ActiveComp: 'app.project.activeItem',
+    Project: 'app.project',
+    Layer: (num: number) => `app.project.activeItem.layer(${num})`
 }
 
 export function aex() {
@@ -18,29 +19,29 @@ export function aex() {
             return await getEvalScriptResult(`aex.benchmark(aex_args)`, options, { ignoreReturn: false });
         },
 
-        async prescan(aeObject: AeObject, options?: AexOptions): Promise<AexPrescanResult> {
+        async prescan(aeObject: string, options?: AexOptions): Promise<AexPrescanResult> {
             return await getEvalScriptResult<AexPrescanResult>(`aex.prescan(${aeObject}, aex_args)`, options || {}, {
                 ignoreReturn: false,
             });
         },
 
-        async get(aeObject: AeObject, options?: AexOptions): Promise<AexResult> {
-            switch (aeObject) {
-                case AeObject.ActiveComp:
-                case AeObject.Project:
-                    return await getEvalScriptResult<AexResult>(`aex.get(${aeObject}, aex_args)`, options || {}, { ignoreReturn: false });
-                default:
-                    throw new Error(`Unrecognized AE Object - ${aeObject}`);
+        async get(aeObject: string, options?: AexOptions): Promise<AexResult> {
+            const isLayer = aeObject.match(/app.project.activeItem.layer\(\d+\)/gi);
+
+            if (isLayer || aeObject == AeObject.ActiveComp || aeObject == AeObject.Project) {
+                return await getEvalScriptResult<AexResult>(`aex.get(${aeObject}, aex_args)`, options || {}, { ignoreReturn: false });
+            } else {
+                throw new Error(`Unrecognized AE Object - ${aeObject}`);
             }
         },
 
-        async create(aeObject: AeObject, aexObject: AexObject, options?: AexOptions): Promise<AexResult> {
+        async create(aeObject: string, aexObject: AexObject, options?: AexOptions): Promise<AexResult> {
             return await getEvalScriptResult<AexResult>(`aex.create(${aeObject}, ${JSON.stringify(aexObject)}, aex_args)`, options || {}, {
                 ignoreReturn: false,
             });
         },
 
-        async update(aeObject: AeObject, aexObject: AexObject, options?: AexOptions): Promise<AexResult> {
+        async update(aeObject: string, aexObject: AexObject, options?: AexOptions): Promise<AexResult> {
             return await getEvalScriptResult<AexResult>(`aex.update(${aeObject}, ${JSON.stringify(aexObject)}, aex_args)`, options || {}, {
                 ignoreReturn: false,
             });
