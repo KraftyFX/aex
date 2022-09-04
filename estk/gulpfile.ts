8961f1fd7d1afcbabc31f41f0a193ffd6f8d0276
Rafi Khan <rafikhan@gmail.com>
2022-09-04 10:09:17 -0700

    Adding release target

--- estk/gulpfile.ts
+++ estk/gulpfile.ts
@@ -8,18 +8,20 @@ const footer = require(`gulp-footer`);
 const paths = {
     src: './src',
     lib: './lib',
+    sample: './sample',
     _build: './_build',
     dist: './dist',
+    release: './release',
 };
 
 function cleanBuild() {
-    return del([`${paths._build}/*`, `${paths.dist}/*`], {
+    return del([`${paths._build}/*`, `${paths.dist}/*`, `${paths.release}/*`], {
         force: true,
         dot: true,
     });
 }
 
-function buildAexBaseLibrary() {
+function buildAexSourceFiles() {
     const tsProject = ts.createProject(`${paths.src}/tsconfig.json`);
 
     return gulp
@@ -28,7 +30,7 @@ function buildAexBaseLibrary() {
         .pipe(gulp.dest(`${paths._build}`));
 }
 
-function buildAexJsx() {
+function assembleAexJsx() {
     const globalVarName = 'aex';
     const filename = `${globalVarName}.jsx`;
     const aexFiles = `./**/!(exports|ipc)*.jsx`;
@@ -39,14 +41,28 @@ function buildAexJsx() {
     return gulp
         .src(baseLibraryFiles, { cwd: paths._build })
         .pipe(concat(filename))
-        .pipe(header(`// (c) 2021 KraftyFX <rafikhan@kraftyfx.com>\nvar ${globalVarName}=(function(_export_) {\n`))
+        .pipe(header(`// (c) 2021 KraftyFX <rafikhan@kraftyfx.com>\nvar ${globalVarName}=(function() {\n`))
         .pipe(footer(`\nreturn _export_;\n})({});`))
         .pipe(gulp.src([aexIpc], { cwd: paths._build }))
         .pipe(concat(filename))
         .pipe(gulp.dest(paths.dist));
 }
 
-const buildAex = gulp.series(buildAexBaseLibrary, buildAexJsx);
+function copyLibs() {
+    return gulp.src(`${paths.lib}/*.jsx`, { cwd: '.' }).pipe(gulp.dest(`${paths.release}/libs`));
+}
+
+function copyAex() {
+    return gulp.src(`${paths.dist}/*.jsx`, { cwd: '.' }).pipe(gulp.dest(paths.release));
+}
+
+function copySampleAssets() {
+    return gulp.src(`${paths.sample}/*`, { cwd: '.' }).pipe(gulp.dest(paths.release));
+}
+
+const buildRelesae = gulp.parallel(copyAex, copyLibs, copySampleAssets);
+
+const buildAex = gulp.series(buildAexSourceFiles, assembleAexJsx);
 
 function startAex() {
     return gulp.watch([`**/*.tsx`, `**/*.d.ts`], { cwd: paths.src }, buildAex);
@@ -54,4 +70,5 @@ function startAex() {
 
 export const clean = gulp.series(cleanBuild);
 export const build = gulp.series(clean, buildAex);
+export const release = gulp.series(clean, buildAex, buildRelesae);
 export const start = gulp.series(build, startAex);
